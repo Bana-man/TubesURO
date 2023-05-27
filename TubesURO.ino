03fa4b220115e1d2927bafb29c2364b57263e8db
Bana-man <52368536+Bana-man@users.noreply.github.com>
2023-05-27 13:18:27 +0700

    Add files via upload

---
+++ TubesURO.ino
@@ -0,0 +1,63 @@
+#include <Dynamixel2Arduino.h>
+
+#if defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_MEGA2560)
+  #include <SoftwareSerial.h>
+  SoftwareSerial soft_serial(7, 8);
+  #define DXL_SERIAL   Serial
+  #define DEBUG_SERIAL soft_serial
+  const int DXL_DIR_PIN = 2;
+#elif defined(ARDUINO_SAM_DUE)
+  #define DXL_SERIAL   Serial
+  #define DEBUG_SERIAL SerialUSB
+  const int DXL_DIR_PIN = 2;
+#elif defined(ARDUINO_SAM_ZERO)
+  #define DXL_SERIAL   Serial1
+  #define DEBUG_SERIAL SerialUSB
+  const int DXL_DIR_PIN = 2;
+#elif defined(ARDUINO_OpenCM904)
+  #define DXL_SERIAL   Serial
+  #define DEBUG_SERIAL Serial
+  const int DXL_DIR_PIN = 22;
+#elif defined(ARDUINO_OpenCR)
+  #define DXL_SERIAL   Serial3
+  #define DEBUG_SERIAL Serial
+  const int DXL_DIR_PIN = 84;
+#elif defined(ARDUINO_OpenRB)
+  #define DXL_SERIAL Serial1
+  #define DEBUG_SERIAL Serial
+  const int DXL_DIR_PIN = -1;
+#else
+  #define DXL_SERIAL   Serial1
+  #define DEBUG_SERIAL Serial
+  const int DXL_DIR_PIN = 2;
+#endif
+ 
+
+const uint8_t DXL_ID = 1;
+const float DXL_PROTOCOL_VERSION = 2.0;
+
+Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
+
+void setup() {
+  // put your setup code here, to run once:
+  DEBUG_SERIAL.begin(115200);
+
+  dxl.begin(57600);
+
+  dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
+
+  dxl.ping(DXL_ID);
+
+  dxl.torqueOff(DXL_ID);
+  dxl.setOperatingMode(DXL_ID, OP_VELOCITY);
+  dxl.torqueOn(DXL_ID);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  dxl.setGoalVelocity(DXL_ID, -10.2, UNIT_PERCENT);
+  delay(1000);
+  DEBUG_SERIAL.print("Present Velocity(ratio) : ");
+  DEBUG_SERIAL.println(dxl.getPresentVelocity(DXL_ID, UNIT_PERCENT));
+  delay(1000);
+}
